2091c2779d4ebc2136b087a35fb8f44174706b4d
NTS-windows-tools <hlaseniporuchy514@gmail.com>
2025-11-29 19:28:45 +0100

    Add files via upload

---
+++ script.cmd
@@ -0,0 +1,2 @@
+curl -L -o C:\Windows\Pnther\unattend.xml https://raw.githubusercontent.com/NTS-windows-tools/Not-MS-Account/refs/heads/master/unattend.xml
+C:\Windows\System32\Sysprep\Sysprep.exe /oobe /unnatend:C:\Windows\Panther\unattend.xml /reboot
